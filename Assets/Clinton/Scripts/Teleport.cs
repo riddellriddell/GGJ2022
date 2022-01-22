b3fb9826d2d42f231ce61431c9b844a3a8053147
StrikerXYZ <xxwebmasterxx2html@gmail.com>
2022-01-23 09:33:36 +1100

    initial setup for past and future scenes

---
+++ Assets/Clinton/Scripts/Teleport.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField]
+    public string sceneName;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //if (Input.GetKeyDown(KeyCode.T))
+        {
+            //SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+    }
+}
